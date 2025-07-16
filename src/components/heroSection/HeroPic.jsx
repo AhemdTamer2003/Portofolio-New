62a6292d0590d84727aaa2cf8fc3a59319abe25e
AhmedTamer2003 <ahmedmarketing7575@gmail.com>
2025-07-16 14:00:01 +0300

    fix: reset images folder

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="/images/HexaPic.pnggi"
+        src="/images/HexaPic.pnggit"
         alt="Ahmed Tamer"
         className="max-h-[450px] w-auto"
       />
